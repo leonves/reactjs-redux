76d7e0adf1c32b81e3667ea203b3dff71aac93a1
Leonardo Neves <neves@MacBook-Pro-de-Leonardo.local>
2019-11-23 20:08:55 -0300

    reducer de todo

---
+++ app03/src/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+
+const TodoList = (props) => {
+    console.log(props)
+    return (
+            <ul>
+                { props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+            </ul>
+
+    );
+
+};
+
+const mapStateToProps = state => ({
+    todos: state.todos,
+})
+
+export default connect(mapStateToProps)(TodoList);
