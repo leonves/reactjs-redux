77f847fd45eecb6a9d5bdf95e41b6cbbf3a70d19
Leonardo Neves <neves@MacBook-Pro-de-Leonardo.local>
2019-11-23 20:20:17 -0300

    action addTodo

--- app03/src/TodoList.js
+++ app03/src/TodoList.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React , { Fragment } from 'react';
 
 import { connect } from 'react-redux';
 
-const TodoList = (props) => {
-    console.log(props)
-    return (
-            <ul>
-                { props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
-            </ul>
+const TodoList = ({todos, addTodo}) => (
+ 
+    <Fragment>
 
-    );
-
-};
+        <ul>
+            { todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+        </ul>
+        <button onClick={() => addTodo('Novo Todo')}>Adicionar</button>
+    </Fragment>
+);
 
 const mapStateToProps = state => ({
     todos: state.todos,
 })
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => ({
+    addTodo: text => dispatch({
+        type: 'ADD_TODO',
+        payload: {
+            text
+        } 
+    }),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
