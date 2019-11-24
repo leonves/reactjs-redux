202466a2d7eae356b4e5958b9e6b9b01c384f774
Leonardo Neves <neves@MacBook-Pro-de-Leonardo.local>
2019-11-24 19:32:05 -0300

    utilizando redux-saga e configuração do reactotron

---
+++ app03/src/pages/main/index.js
@@ -0,0 +1,56 @@
+import React , { Fragment, Component } from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as FavoriteActions from '../../store/actions/favorites';
+
+
+class Main extends Component {
+    state = {
+        repositoryInput: '',
+    }
+
+    handleAddRepository = (event) =>
+    {
+        event.preventDefault();
+        this.props.addFavoriteRequest(this.state.repositoryInput);
+    }
+    
+    render() {
+        return (
+            <Fragment>
+                <form onSubmit={this.handleAddRepository}>
+                    <input 
+                    placeholder="usuario/repositório"
+                    value = {this.state.repositoryInput}
+                    onChange = {e => this.setState({ repositoryInput: e.target.value })}
+                    />
+                    <button type="submit">Adicionar</button>
+                </form>
+                <ul>
+                    {
+                        this.props.favorites.map(favorite => (
+                            <li key={favorite.id}>
+                                <p>
+                                    <strong>{favorite.name}</strong> ({favorite.description})
+                                </p>
+                                <a href={favorite.url}>Acessar</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </Fragment>
+        )
+    }
+}
+
+const mapStateToProps = state => (
+    {
+        favorites: state.favorites,
+    }
+);
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- app03/src/store/index.js
+++ app03/src/store/index.js
@@ -1,7 +1,24 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import reducers from'./reducers';
+import sagas from './sagas';
 
-const store = createStore(reducers);
+const middlewares = [];
+
+const sagaMiddlewares = createSagaMiddleware();
+middlewares.push(sagaMiddlewares);
+
+const composer = process.env.NODE_ENV === 'development' 
+    ? compose (
+        applyMiddleware(... middlewares),
+        console.tron.createEnhancer()
+
+    )
+    : applyMiddleware(...middlewares)
+
+const store = createStore(reducers, composer);
+
+sagaMiddlewares.run(sagas);
 
 export default store;
